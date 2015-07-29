baa5bf6e5106fef8c23ef964528579158a7c2a3b
VidyashreeBL <vidu_friend@hotmail.com>
2015-07-29 15:58:08 +0530

    Merging addWhiteMove and addBlackMove

--- PGNTracer.java
+++ PGNTracer.java
@@ -10,7 +10,8 @@ public class PGNTracer {
         PAWN, KING, QUEEN, ROOKS, BISHOP, KNIGHT;
     }
     
-    public void makeWhiteMove(String move) {
+    public void makeMove(char color, String move) {
+        
         int posi;
         int posj;
         int posNewi;
@@ -20,12 +21,17 @@ public class PGNTracer {
             posNewi = getRowIndex(move);
             posNewj = getColumnIndex(move);
             if(findPiece(move)==Piece.PAWN){
-                
-                //Itereate through all black pawns
                 for(int i = 1; i < 9; i++){
-                    posi =  chessBoard.pieceToIndex.get("p"+i)[0];
-                    posj =  chessBoard.pieceToIndex.get("p"+i)[1];
-                    if(chessBoard.isValidPawnMove(posi, posj, posNewi, posNewj)){    
+                    if(color == 'W'){
+                        posi =  chessBoard.pieceToIndex.get("p"+i)[0];
+                        posj =  chessBoard.pieceToIndex.get("p"+i)[1];
+                    }
+                    else{
+                        posi =  chessBoard.pieceToIndex.get("P"+i)[0];
+                        posj =  chessBoard.pieceToIndex.get("P"+i)[1];
+                    }
+                        
+                    if(chessBoard.isValidPawnMove(color,posi, posj, posNewi, posNewj)){    
                         chessBoard.updateChessBoard(posi, posj, posNewi , posNewj );
                     }
                     
