f1d81a695fe11efd6124608407f5fea56ea93b4b
BalajiAmarnath <balajigunyacloud@gmail.com>
2015-07-29 12:33:40 +0530

    Added member variables and methods(APIS)

--- PGNTracer.java
+++ PGNTracer.java
@@ -1,3 +1,17 @@
 public class PGNTracer {
+    private ChessBoard chessBoard;
+    
+    public PGNTracer() {
+        chessBoard = new chessBoard(); 
+    }
+    
+    public void makeMove(String move) {
+        
+    }
+    
+    public void displayBoard() {
+        
+    }
+    
     
 }
