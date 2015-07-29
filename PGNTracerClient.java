ab1fd3d52a60a3dbb5fc0156c79c508714ba81a5
BalajiAmarnath <balajigunyacloud@gmail.com>
2015-07-29 13:31:36 +0530

    Added the main function

--- PGNTracerClient.java
+++ PGNTracerClient.java
@@ -1,11 +1,18 @@
 package PGNTracer;
 
 class PGNTracerClient {
-	String moves = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6";
-
+	public static String moves = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6";
+	
 	public static void main(String[] args) {
 		PGNTracer pgnTracer = new PGNTracer();
 		// Reads the file move by move and calls the addMove function in PGNTracer
+		
+		String[] splits = moves.split(" ");
+		for(int i=0 ; i< splits.length ; i = i+3 ) {
+			pgnTracer.makeWhiteMove(split[i+1]);
+			pgnTracer.makeBlackMove(split[i+2]);
+		}
+		
 
 	}
 
