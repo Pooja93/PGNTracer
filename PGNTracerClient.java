f6a983454d0b4f343e1afa658544229691eb9a33
PoojaSB93 <poojasb2093@gmail.com>
2015-07-29 12:36:40 +0530

    Added the function Template

--- PGNTracerClient.java
+++ PGNTracerClient.java
@@ -1,4 +1,12 @@
-class PGNTracerClient {
+package PGNTracer;
 
+class PGNTracerClient {
+	String moves = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6";
+
+	public static void main(String[] args) {
+		PGNTracer pgnTracer = new PGNTracer();
+		// Reads the file move by move and calls the addMove function in PGNTracer
+
+	}
 
 }
